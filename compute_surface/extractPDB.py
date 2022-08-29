20c91e429da10b5d8b637067a9065dccd478f017
zpliulab <54654413+zpliulab@users.noreply.github.com>
2022-08-30 01:54:02 +0800

    Update extractPDB.py
    add support PDB format.

--- compute_surface/extractPDB.py
+++ compute_surface/extractPDB.py
@@ -11,19 +11,28 @@ class NotDisordered(Select):
     def accept_atom(self, atom):
         return not atom.is_disordered() or atom.get_altloc() == "A"  or atom.get_altloc() == "1"
 # Exclude disordered atoms.
-def extractPDB(pair):
 
+def extractPDB(pair, raw_pbd_format='pdb'):
     pairs = pair.split('_')
     pdb_id = pairs[0]
-    protein_chain = pairs[1]
-    infilename = os.path.join(dir_opts['raw_pdb_dir'],pdb_id+'.cif')
+    if len(pairs)==1:
+        protein_chain=''
+    else:
+        protein_chain = pairs[1]
     if not os.path.exists(dir_opts['chain_pdb_dir']):
         os.mkdir(dir_opts['chain_pdb_dir'])
 
     outfilename = os.path.join(dir_opts['chain_pdb_dir'], pair+'.pdb')
     # extract the chain_ids from infilename and save in outfilename.
-
-    parser = MMCIFParser(QUIET=True)
+    if raw_pbd_format=='cif':
+        infilename=os.path.join(dir_opts['raw_pdb_dir'], pdb_id + '.cif')
+        parser = MMCIFParser(QUIET=True)
+    elif raw_pbd_format=='pdb':
+        infilename=os.path.join(dir_opts['raw_pdb_dir'], pdb_id + '.pdb')
+        parser = PDBParser(QUIET=True)
+    else:
+        print('not supported format')
+        exit()
     struct = parser.get_structure(infilename, infilename)
     model = Selection.unfold_entities(struct, "M")[0]
     # Select residues to extract and build new structure
@@ -34,7 +43,7 @@ def extractPDB(pair):
     outputStruct = structBuild.get_structure()
     RNA_index = 0
     for chain in model:
-        if chain.get_id()==protein_chain:
+        if chain.get_id()==protein_chain or protein_chain=='':
             structBuild.init_chain(chain.get_id()[-1])
             for residue in chain:
                 if residue.get_resname().upper() in kd_scale.keys():
