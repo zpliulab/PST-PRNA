e1e09752486e4e9c5b6cc6662dd5416c9236e4c9
zpliulab <54654413+zpliulab@users.noreply.github.com>
2022-08-30 01:51:26 +0800

    Update prepare_all.py

--- prepare_all.py
+++ prepare_all.py
@@ -1,10 +1,10 @@
 import os, sys
-from protein import RBP
-import numpy as np
+from protein_for_predict import RBP
+import shutil
 from default_config.dir_options import dir_opts
 if __name__ == "__main__":
-    dataset_path = 'data/pdbid_chain/RBP03.txt'
-    #dataset_path = 'data/pdbid_chain/RBP407_09.txt'
+
+    dataset_path = dir_opts['PDB_list_to_predict']
 
     protein_list = []
     with open(dataset_path, 'r') as pid:
@@ -14,12 +14,14 @@ if __name__ == "__main__":
     if not os.path.exists(dir_opts['data_label']):
         os.mkdir(dir_opts['data_label'])
 
+
     # pdb_slurm = sys.argv[1].strip()
     print(len(protein_list))
     for index, item in enumerate(protein_list):
-        # if item == pdb_slurm:
-            if os.path.exists(os.path.join(dir_opts['data_label'], item)):
-                continue
-            print(index, item)
-            rbp = RBP(item)
-            rbp.get_data()
+        # if item != pdb_slurm:
+        #    continue
+        if os.path.exists(os.path.join(dir_opts['data_label'], item)):
+            continue
+        print(index, item)
+        rbp = RBP(item)
+        rbp.get_data()
